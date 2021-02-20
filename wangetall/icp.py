e00ec19a6e50ef341b066a22529f24e9c3b6fb8e
smejkka3 <karel.smejkal@campus.tu-berlin.de>
2021-02-20 18:00:46 +0100

    changed params

--- wangetall/icp.py
+++ wangetall/icp.py
@@ -18,19 +18,18 @@ class ICP:
                                               transformation to be considered converged
     :param convergence_rotation_threshold: the threshold for the rotation angle (in rad) for the transformation
                                                to be considered converged
-    :param point_pairs_threshold: the minimum number of point pairs the should exist
+    :param point_pairs_threshold: the minimum number of point pairs that should exist
     :return: aligned points as a numpy array M x 2
     """
     def __init__(self):
         """
-        Testing out with the default params from  https://github.com/richardos/icp/blob/master/icp.py, probably
-        needs to be adjusted later.
+        Testing out with the params
         """
         self.max_iterations=100
-        self.distance_threshold=0.3
+        self.distance_threshold=1
         self.convergence_translation_threshold=1e-3
         self.convergence_rotation_threshold=1e-4
-        self.point_pairs_threshold=10
+        self.point_pairs_threshold=5
 
     def run(self, reference_points, points):
         self.reference_points = reference_points
@@ -38,21 +37,26 @@ class ICP:
         nbrs = NearestNeighbors(n_neighbors=1, algorithm='kd_tree').fit(self.reference_points)
 
         for iter_num in range(self.max_iterations):
+            print('------ iteration', iter_num, '------')
+
             closest_point_pairs = []  # list of point correspondences for closest point rule
 
             distances, indices = nbrs.kneighbors(self.points)
             for nn_index in range(len(distances)):
-                if distances[nn_index][0] < self.distance_threshold:
+                if distances[nn_index][0] < self.distance_threshold: # ELSE OUTLIER?
                     closest_point_pairs.append((self.points[nn_index], self.reference_points[indices[nn_index][0]]))
 
             # if only few point pairs, stop process
+            print('number of pairs found:', len(closest_point_pairs))
             if len(closest_point_pairs) < self.point_pairs_threshold:
+                print('No better solution can be found (very few point pairs)!')
                 break
 
             # compute translation and rotation using point correspondences
             closest_rot_angle, closest_translation_x, closest_translation_y = self.point_based_matching(closest_point_pairs)
 
             if closest_rot_angle is None or closest_translation_x is None or closest_translation_y is None:
+                print('No better solution can be found!')
                 break
 
             # transform 'points' (using the calculated rotation and translation)
@@ -70,6 +74,7 @@ class ICP:
             if (abs(closest_rot_angle) < self.convergence_rotation_threshold) \
                     and (abs(closest_translation_x) < self.convergence_translation_threshold) \
                     and (abs(closest_translation_y) < self.convergence_translation_threshold):
+                print('Converged!')
                 break
 
         return self.points
