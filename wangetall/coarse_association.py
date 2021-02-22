f89ff8aea88c83e0547a25f8cc28e2ccc6c235cd
smejkka3 <karel.smejkal@campus.tu-berlin.de>
2021-02-22 09:03:48 +0100

    more testing

--- wangetall/coarse_association.py
+++ wangetall/coarse_association.py
@@ -18,29 +18,35 @@ if __name__ == "__main__":
     cl = cluster.Cluster()
 
     # set seed for reproducible results
-    random.seed(1234)
+    random.seed(123)
 
-    # create a set of points to be the reference for ICP
-    Z = np.array(random.sample(range(200), 200)).reshape((100,2))
+
+    Z = np.array(random.sample(range(100), 100)).reshape((50,2))
     C = cl.cluster(Z)
 
     n_boundary = 10
+    # create a set of points to be the reference for ICP
     Q = np.zeros((n_boundary,2))
     Q[:,0] = np.array([10,20,30,40,50,60,60,60,60,60])
     Q[:,1] = np.array([60,60,60,60,60,50,40,30,20,10])
 
     static_C = associateAndUpdateWithStatic(C, Z, Q)
+    print("clusters:", len(C))
+    print("static clusters:", len(static_C))
 
     for key in C.keys():
         P = Z[C[key]]
         plt.scatter(P[:,0], P[:,1])
     plt.show()
 
+    data = plt.scatter(Z[:,0],Z[:,1])
+
+
     for key in static_C.keys():
         static_P = Z[static_C[key]]
-        data = plt.scatter(Z[:,0],Z[:,1])
-        static_cloud = plt.scatter(static_P[:,0],static_P[:,1])
+        static_cloud = plt.scatter(static_P[:,0],static_P[:,1], marker='D')
         border = plt.scatter(Q[:,0],Q[:,1], marker='x')
         plt.legend((data, static_cloud, border),
                     ("laser scan data", "static cloud", "boundary points"))
+
     plt.show()
